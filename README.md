5b37f6484511e98108be4de87b588a8e5a9ba604
Michael <michaelwoo921@gmail.com>
2020-02-15 10:26:44 -0800

    compo props 2

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # master-react-redux
 
 # Complete React Redux codes
+
+#Start in the folowing order
+1) jsx
+2) compo (reference: semantic ui, faker.js, )
--- compo/src/index.js
+++ compo/src/index.js
@@ -3,34 +3,39 @@ import ReactDOM from "react-dom";
 import faker from "faker";
 
 
-function CommentDetail(){
+function CommentDetail(props){
+    console.log(props);
     return (
         <div className="comment">
         <a className="avatar" href="/">
-            <img alt="avatar" src={faker.image.avatar()}/>
+            <img alt="avatar" src={props.avatar}/>
         </a>
         <div className="content">
-          <a className="author">Matt</a>  
+          <a className="author">{props.author}</a>  
         
             <div className="metadata">
-            <span className="date">Today at 5:42PM</span>
+            <span className="date">{props.timeAgo} </span>
             </div>    
             <div className="text">
-                A great post!
+                {props.content}
             </div>
         </div>
     </div>
     );
 }
-function App(){
-    return (
-        <div className="ui container comments">
-           <CommentDetail />
-           <CommentDetail />
-           <CommentDetail />
-           <CommentDetail />
-        </div>
-    );
+class App extends React.Component {
+
+    render(){
+       
+        return (
+            <div className="ui container comments">
+               <CommentDetail author="Alex" content ="a great post!" timeAgo="Today 4:00PM" avatar={faker.image.avatar()}/>
+               <CommentDetail author="Tom" content ="amazing" timeAgo="Today 6:30AM" avatar={faker.image.avatar()}/>
+               <CommentDetail author="Jane" content="I love this post" timeAgo="Yesterday 7:45PM" avatar={faker.image.avatar()}/>
+            </div>
+        );
+    }
+  
 }
 
 ReactDOM.render(<App />,
