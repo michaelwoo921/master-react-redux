0d9d77b473590b93cd90e223a4a142749325b7e1
Michael <michaelwoo921@gmail.com>
2020-02-15 01:24:17 -0800

    jsx-input form

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -5,7 +5,17 @@ import ReactDOM from "react-dom";
 // create a componenent functional component or class componenent
 
 function App(){
-    return <div> Hi there </div>;
+    // use JS based style, put js var inside curly braces
+    let btnText="submit";
+    return (
+        <form className="form" action="">
+        <label htmlFor="name">Name </label>
+        <input id="name" type="text"/>
+        <button style={{backgroundColor: 'blue', color: 'white', padding: '10px' }}>{btnText} </button>
+      </form>
+    )
+   
+
 }
 
 // show the component in a browser
